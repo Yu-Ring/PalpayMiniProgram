967ac45daec0ab0dd7c1c32fe9a89e3240592a0d
“LandingGuyEYL” <1548866254@qq.com>
2023-11-09 18:36:23 +0800

    git push

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 App({
+  todos: [
+    { text: '失眠', completed: true },
+    { text: '不快乐', completed: true },
+    { text: '眼睛疼', completed: false },
+    { text:'喉咙痛',completed:true},
+  ],
+  userInfo: null,
+
+  getUserInfo() {
+    return new Promise((resolve, reject) => {
+      if (this.userInfo) resolve(this.userInfo);
+
+      my.getAuthCode({
+        scopes: ['auth_user'],
+        success: authcode => {
+          console.info(authcode);
+
+          my.getAuthUserInfo({
+            success: res => {
+              this.userInfo = res;
+              resolve(this.userInfo);
+            },
+            fail: () => {
+              reject({});
+            },
+          });
+        },
+        fail: () => {
+          reject({});
+        },
+      });
+    });
+  },
   onLaunch(options) {
     // 第一次打开
     // options.query == {number:1}
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,10 @@
 Page({
   data: {},
   onLoad() {},
+  longin(){
+    my.switchTab({ url: '../index/index' });
+  },
+  register(){
+    my.navigateTo({url:'../register_collect_sympotom/register_collect_sympotom'});
+  }
 });
