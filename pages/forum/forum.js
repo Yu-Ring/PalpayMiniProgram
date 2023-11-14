d7eae61523959889446d70dc70d0aae0442e6b05
Yu-Ring <1807169624@qq.com>
2023-11-15 01:09:09 +0800

    论坛查看、具体文章查看、具体文章评论页面。文章发布页面（和前者尚未做交互）

---
+++ pages/forum/forum.js
@@ -0,0 +1,56 @@
+Page({
+  data: {
+    // 示例数据 -- 具体请根据 数据字典填写
+    articles: [
+      {
+        id: '1',
+        title: '文章标题1',
+        summary: '这里是文章摘要1...',
+        imageUrl: '/images/琉璃龙.jpg',
+        author: '作者1',
+        time: '2021-01-01'
+      },
+      {
+        id: '2',
+        title: '文章标题2',
+        summary: '这里是文章摘要2...',
+        imageUrl: '/images/铁道-列车.png',
+        author: '作者2',
+        time: '2021-02-01'
+      },
+      {
+        id: '3',
+        title: '文章标题3',
+        summary: '这里是文章摘要3...',
+        imageUrl: '/images/Mimikyu.jpg',
+        author: '作者1',
+        time: '2021-01-01'
+      },
+      // ... 更多文章
+    ]
+  },
+  onLoad() {
+    // 从后端获取文章数据
+    // my.request({
+    //   url: '后端接口URL',
+    //   method: 'GET',
+    //   dataType: 'json',
+    //   success: (res) => {
+    //     this.setData({ articles: res.data });
+    //   },
+    //   fail: (err) => {
+    //     console.error('请求失败:', err);
+    //   }
+    // });
+  },
+  goToDetailPage(event) {
+    // 获取被点击的文章ID
+    const articleId = event.currentTarget.dataset.id;
+
+    // 这里实现跳转逻辑
+    // 例如，跳转到文章详情页，并传递文章ID
+    my.navigateTo({
+      url: `/pages/forum_detail/forum_detail=${articleId}`
+    });
+  },
+});
